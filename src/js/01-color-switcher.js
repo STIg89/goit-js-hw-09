6a0cfb9bfc479ce21d807096cfdcefe930deace8
STIg89 <andreystig89@gmail.com>
2022-11-15 22:43:56 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onClickStart);
+refs.stopBtn.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStop() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
